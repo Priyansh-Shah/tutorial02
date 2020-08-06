00719569b24002f53b6785d64c5cee8791da82d4
Priyansh-Shah <priyanshshah156pr@gmail.com>
2020-08-06 16:24:36 +0530

    group project

---
+++ app/src/main/java/com/example/tutorial02/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tutorial02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
